381b866b81db50b880ec62b5f8b7667009be0d4f
louie camacho dxa <louiecamacho@gmail.com>
2022-02-24 10:44:10 -0600

    components in place

---
+++ my-app/src/components/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Cards() {
+	return (
+		<div className="card">
+			<img src="#" alt="" />
+			<div className="card--info">
+				<div className="card--location">location</div>
+				<div className="card--name">name</div>
+				<div className="card--date">date</div>
+				<div className="card--description">description</div>
+			</div>
+		</div>
+	);
+}
+
+export default Cards;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+	return (
+		<nav>
+			{/* <img src={require(`../images/pin.svg`)} alt="" /> */}
+			navbar
+		</nav>
+	);
+}
+
+export default Navbar;
