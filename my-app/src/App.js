a0990759cc090b81bf71edeb114d8264b6bc8075
louie camacho dxa <louiecamacho@gmail.com>
2022-02-24 11:23:51 -0600

    data and props in place

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Cards from './components/Cards';
+import data from './data';
 
 function App() {
+	const travels = data.map((place) => {
+		// console.log(place);
+		return <Cards {...place} />;
+	});
 	return (
 		<div className="App">
 			<Navbar />
-			<Cards />
+			{travels}
 		</div>
 	);
 }
--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-function Cards() {
+function Cards(props) {
 	return (
 		<div className="card">
-			<img src="#" alt="" />
+			<img src={props.imageUrl} alt="" />
 			<div className="card--info">
-				<div className="card--location">location</div>
-				<div className="card--name">name</div>
-				<div className="card--date">date</div>
-				<div className="card--description">description</div>
+				<div className="card--location">
+					{props.location} <a href={props.googleMapsUrl}>View on Google Maps</a>
+				</div>
+				<div className="card--name">{props.title}</div>
+				<div className="card--date">{props.startDate}</div>
+				<div className="card--date">{props.endDate}</div>
+				<div className="card--description">{props.description}</div>
 			</div>
 		</div>
 	);
